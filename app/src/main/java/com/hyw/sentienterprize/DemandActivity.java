e7a7eb7351b835c24ad20936002ddd2ad2ebcb0e
geraldhyw <gerald5jan@yahoo.com.sg>
2020-11-17 19:40:08 +0800

    first commit

---
+++ app/src/main/java/com/hyw/sentienterprize/DemandActivity.java
@@ -0,0 +1,14 @@
+package com.hyw.sentienterprize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demand);
+    }
+}
---
+++ app/src/main/java/com/hyw/sentienterprize/POSActivity.java
@@ -0,0 +1,14 @@
+package com.hyw.sentienterprize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class POSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_o_s);
+    }
+}
